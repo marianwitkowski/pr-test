a1884f47595d52e91fb65e48855443c9b87fe2d8
Marian Witkowski <marian.witkowski@gmail.com>
2021-04-20 12:13:35 +0200

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,14 @@
+from math import pi
+
+def calc_circle_area(r):
+    if not type(r) in [int, float]:
+        raise TypeError("invalid radius type")
+    if r<0:
+        raise ValueError("Radius must be positive value")
+    return pi*(r**2)
+
+"""
+radius = [1, 2, -1, 0]
+for r in radius:
+    print(f"r={r} area={calc_circle_area(r)}")
+"""
